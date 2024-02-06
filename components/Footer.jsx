508a56ed3aad168f74e55abf36033b3fc7bde04e
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-02-06 11:25:51 +0100

    remove

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,9 +21,8 @@ export default function Footer(
   }, [inputValue])
 
   return (
-    <footer className={`fixed z-[5] w-full bottom-0 left-0 
-      backdrop-blur-[100px] bg-white/50 px-3 py-3 
-      ${isTyping ? 'fixed bottom-0 left-0 z-10' : ''}`
+    <div className={`fixed z-[5] w-full bottom-0 left-0 
+      backdrop-blur-[100px] bg-white/50 px-3 py-3`
     }>
       <div className='flex items-center gap-2'>
         <textarea
@@ -61,6 +60,6 @@ export default function Footer(
             
           </section> */}
           
-    </footer>
+    </div>
   )
 }
