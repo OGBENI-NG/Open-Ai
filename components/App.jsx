42842358df044604020d4f73fa9d3650295566f2
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-02-06 12:44:05 +0100

    fin

--- components/App.jsx
+++ components/App.jsx
@@ -127,7 +127,7 @@ export default function App() {
 
   return (
     <main className={`font-roboto min-h-screen overflow-x-hidden 
-      ${userThemes} ${isTyping ? 'fixed' : 'relative'}`
+      ${userThemes} ${isTyping ? 'overflow-hidden' : 'relative'}`
     }>
       <Header
         headerBg={headerBg}
