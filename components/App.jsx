f38b6538dec82a4d81d7682915d5823db0338c43
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-02-06 10:37:08 +0100

    try

--- components/App.jsx
+++ components/App.jsx
@@ -128,7 +128,7 @@ export default function App() {
   return (
     <main className={`font-roboto min-h-screen overflow-x-hidden 
       ${userThemes}
-      ${isTyping ? 'fixed h-screen' : 'relative'}`
+      ${isTyping ? 'fixed h-screen overflow-x-hidden' : 'relative'}`
     }>
       <Header
         headerBg={headerBg}
