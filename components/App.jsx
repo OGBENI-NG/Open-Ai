ac58624c00eb46bee078fe131d881c6340b49ae3
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-02-07 14:10:02 +0100

    try

--- components/App.jsx
+++ components/App.jsx
@@ -127,7 +127,7 @@ export default function App() {
 
   return (
     <main className={`font-roboto scroll-smooth h-screen overflow-y-hidden
-      flex flex-col bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-300 via-red-600 to-fuchsia-700
+      flex flex-col bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-300 via-red-600 to-fuchsia-700 m-0
       `
     }>
      <div className='overflow-scroll'>
--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,7 @@ export default function Main({ renderAiResponse, userChat, loading, containerRef
       <section className='flex flex-col gap-5'>
         <section 
           ref={containerRef}
-          className={`px-3 py-[70px] h-auto overflow-y-hidden`}
+          className={`px-3 pt-[70px] h-auto overflow-y-hidden`}
         >
           <h1 className={`text-center font-[900] text-sm rounded-[8px] 
             text-[#1e1c28] w-48 tracking-[0.011rem] backdrop-blur-[100px]
