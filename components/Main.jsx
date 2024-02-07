bd689d8e919a54899e7a88360799e47f5e641cce
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-02-07 13:18:18 +0100

    tt

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,7 @@ export default function Main({ renderAiResponse, userChat, loading, containerRef
       <section className='flex flex-col gap-5 relative overflow-hidden'>
         <section 
           ref={containerRef}
-          className={`px-3 py-[70px] h-screen scroll-smooth overflow-x-hidden`}
+          className={`px-3 py-[70px] h-screen scroll-smooth overflow-auto`}
         >
           <h1 className={`text-center font-[900] text-sm rounded-[8px] 
             text-[#1e1c28] w-48 tracking-[0.011rem] backdrop-blur-[100px]
@@ -16,7 +16,7 @@ export default function Main({ renderAiResponse, userChat, loading, containerRef
             me to translate into.
           </h1>
           {userChat.map((txt, i) => (
-            <section key={i} className='transition-all'>
+            <section key={i} className='transition-all '>
               <p className='font-bold text-[20px] rounded-[10px] rounded-tl-[1px]
                 text-bodyBg leading-[26px] bg-black/60 backdrop-blur-[50px]
                 px-4 pt-3 pb-5 
